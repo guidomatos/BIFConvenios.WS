256774a7f938103432b0deb07c2f683fbf219466
GuidoMatos <guido.matos.88@gmail.com>
2023-02-09 15:27:06 -0500

    BusinessLogic project migrated

---
+++ src/BusinessLogic/DLEMP_logic.cs
@@ -0,0 +1,21 @@
+﻿namespace BusinessLogic
+{
+    public class DLEMP_logic
+    {
+        public bool actualizar_dlemp(string cliente_id, string anio, string mes)
+        {
+            bool result;
+            try
+            {
+
+                var dlemp_acc = new DataAccess.DLEMP();
+                result = dlemp_acc.Actualizar_datos_DLEMP(cliente_id, anio, mes);
+            }
+            catch
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
